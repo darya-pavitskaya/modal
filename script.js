a975424886f54973d89ed92e406a22a4d1dcf2ee
Darya Pavitskaya <dpavitskaya@gmail.com>
2022-10-05 21:48:34 +0300

    feat: add basic script

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const toggleModal = () => {
+  modal.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', toggleModal);
+  btnCloseModal.addEventListener('click', toggleModal);
+}
+
+overlay.addEventListener('click', toggleModal);
